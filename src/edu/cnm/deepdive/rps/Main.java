2ffc5071a15f4672e9e52c7c3916183829904fe3
bBleck <bmbleck@hotmail.com>
2018-11-15 11:52:22 -0700

    Added controller, main

---
+++ src/edu/cnm/deepdive/rps/Main.java
@@ -0,0 +1,43 @@
+package edu.cnm.deepdive.rps;
+
+import edu.cnm.deepdive.rps.controller.Controller;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  private static final String RESOURCES_DIR = "resources";
+  private static final String LAYOUT_RESOURCE = RESOURCES_DIR + "/main.fxml";
+  private static final String RESOURCES_BUNDLE = RESOURCES_DIR + "/strings";
+  private static final String WINDOW_TITLE_KEY = "window_title";
+
+  private Controller controller;
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    ClassLoader classLoader = getClass().getClassLoader();
+    ResourceBundle resourceBundle = ResourceBundle.getBundle(RESOURCES_BUNDLE);
+    FXMLLoader fxmlLoader = new FXMLLoader(classLoader.getResource(LAYOUT_RESOURCE), resourceBundle);
+    Parent root = fxmlLoader.load();
+    controller = fxmlLoader.getController();
+    Scene scene = new Scene(root);
+    stage.setTitle(resourceBundle.getString(WINDOW_TITLE_KEY));
+    stage.setResizable(false);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  @Override
+  public void stop() throws Exception {
+    controller.stop(null);
+    super.stop();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
